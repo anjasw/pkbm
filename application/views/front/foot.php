871f5ae18397611a3309f31d7a88dae2d67a7adb
Aprea <apreak@gmail.com>
2022-02-15 16:31:52 +0700

    layout

--- application/views/front/foot.php
+++ application/views/front/foot.php
@@ -1,31 +1,35 @@
-<footer class="py-3 my-0 bg-light">
-<div class="container">
-<div class="row">
-    <div class="col my-auto">
-      <p class="text-muted">&copy; KPT 2022</p>
-    </div>
-
-    <div class="col">
-      <div class="row">
-        <?php foreach($posts->result() as $k): ?>
-          <div class="col-md-4">
-          <div class="card mb-3">
-            <div class="row g-0">
-              <div class="col-md-12">
-                <div class="card-body">
-                  <h5 class="card-title"><a href="<?php echo base_url().'home/detail_post/'.$k->id_posts.'/'.str_replace(' ','-',strtolower($k->title)) ?>"><?php echo $k->title ?></a></h5>
-                  <p class="card-text"><?php echo word_limiter($k->description,20) ?></p>
+  <section id="grad">
+      <div class="container text-light">
+          <div class="row g-4 justify-content-between align-items-center py-3">
+              <div class="col-md-2">
+                <img class="img-fluid" src="<?php echo base_url().'assets/dist/img/logo-pkbm.png'?>" style="width:176px;">
+              </div>
+              <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-4 col-6 order-3 order-md-2">
+                        <h5 class="mb-4 fw-bold">Tentang</h5>
+                        <h6 class="mb-3"><a href="#sec-tentangkami" class="text-light" style="text-decoration: none;">Kontak Kami</a></h6> 
+                    </div>
+                    
+                    <div class="col-md-8 order-4">
+                        <h5 class="mb-4 fw-bold">Kantor Pusat I</h5>
+                        <p>
+                            Jl. Bumi Sentosa Raya A1/5-8, A1/31-33, A3/3, B1/3 Cibinong, Kab. Bogor, Jawa Barat, Indonesia 16912.
+                            <!-- Jl. Bumi Sentosa Raya Blok A3 No.3, A1 No. 5 - 8, Perum. Bumi Sentosa, Cibinong, Jawa Barat 16912, Indonesia -->
+                        </p>
+                        
+                    </div>
                 </div>
               </div>
-            </div>
           </div>
-          </div>
-          <?php endforeach; ?>
-        </div>
-    </div>
-</div>
-</div>
-</footer>
+      </div>
+    </section>
+<section  class="py-3 bg-dark text-light text-center">
+<span>
+    Copyright&copy; Pusat Kegiatan Belajar Masyarakat (PKBM) by Konsultan Pendidikan Tinggi 2022
+</span>
+</section>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <script>
--- pkbm.sql
+++ pkbm.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100418
  File Encoding         : 65001
 
- Date: 07/02/2022 14:36:41
+ Date: 15/02/2022 09:55:37
 */
 
 SET NAMES utf8mb4;
@@ -51,7 +51,7 @@ CREATE TABLE `config_page`  (
 -- ----------------------------
 -- Records of config_page
 -- ----------------------------
-INSERT INTO `config_page` VALUES (1, 'HOME | SMKN 1 CIOMAS', NULL, NULL, 'https://www.google.com/maps/d/embed?mid=1viyZhqtEarcpsBeyR-zqD5yIoBs&ehbc=2E312F', 'pendaftaran online', 'pendaftaran sd.smp.sma,smk');
+INSERT INTO `config_page` VALUES (1, 'HOME | SMKN 1 CIOMAS', 'pkbm_posts_1644290348.jpg', 'pkbm_posts_1644290430.jpg', 'https://www.google.com/maps/d/embed?mid=1viyZhqtEarcpsBeyR-zqD5yIoBs&ehbc=2E312F', 'pendaftaran online', 'pendaftaran sd.smp.sma,smk');
 
 -- ----------------------------
 -- Table structure for jalur_jenjang
@@ -73,21 +73,38 @@ INSERT INTO `jalur_jenjang` VALUES (2, 'sd', '#dc3545', 'Prestasi', '#ffffff');
 INSERT INTO `jalur_jenjang` VALUES (3, 'sd', '#dc3545', 'Pindah Tugas Orangtua dan Anak Guru', '#ffffff');
 INSERT INTO `jalur_jenjang` VALUES (4, 'sd', '#dc3545', 'Tahap Kedua', '#ffffff');
 INSERT INTO `jalur_jenjang` VALUES (5, 'sd', '#dc3545', 'Tahap Ketiga', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (6, 'smp', '#0d6efd', 'Prestasi', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (7, 'smp', '#0d6efd', 'Afirmasi', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (8, 'smp', '#0d6efd', 'Zonasi', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (9, 'smp', '#0d6efd', 'Pindah Tugas Orangtua dan Anak Guru', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (10, 'smp', '#0d6efd', 'Tahap Kedua', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (11, 'sma', '#6c757d', 'Prestasi', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (12, 'sma', '#6c757d', 'Afirmasi', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (13, 'sma', '#6c757d', 'Zonasi', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (14, 'sma', '#6c757d', 'Pindah Tugas Orangtua dan Anak Guru', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (15, 'sma', '#6c757d', 'Tahap Kedua', '#ffffff');
-INSERT INTO `jalur_jenjang` VALUES (16, 'sma', '#6c757d', 'PPDB Bersama Tahap Kedua', '#ffffff');
+INSERT INTO `jalur_jenjang` VALUES (6, 'smp', '#0d6efd', 'Prestasi', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (7, 'smp', '#0d6efd', 'Afirmasi', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (8, 'smp', '#0d6efd', 'Zonasi', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (9, 'smp', '#0d6efd', 'Pindah Tugas Orangtua dan Anak Guru', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (10, 'smp', '#0d6efd', 'Tahap Kedua', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (11, 'sma', '#6c757d', 'Prestasi', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (12, 'sma', '#6c757d', 'Afirmasi', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (13, 'sma', '#6c757d', 'Zonasi', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (14, 'sma', '#6c757d', 'Pindah Tugas Orangtua dan Anak Guru', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (15, 'sma', '#6c757d', 'Tahap Kedua', '#000000');
+INSERT INTO `jalur_jenjang` VALUES (16, 'sma', '#6c757d', 'PPDB Bersama Tahap Kedua', '#000000');
 INSERT INTO `jalur_jenjang` VALUES (17, 'smk', '#ffc107', 'Prestasi', '#000000');
 INSERT INTO `jalur_jenjang` VALUES (18, 'smk', '#ffc107', 'Afirmasi', '#000000');
 INSERT INTO `jalur_jenjang` VALUES (19, 'smk', '#ffc107', 'Pindah Tugas Orangtua dan Anak Guru', '#000000');
 INSERT INTO `jalur_jenjang` VALUES (20, 'smk', '#ffc107', 'Tahap Kedua', '#000000');
+
+-- ----------------------------
+-- Table structure for kategori
+-- ----------------------------
+DROP TABLE IF EXISTS `kategori`;
+CREATE TABLE `kategori`  (
+  `id_kategori` int NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_kategori`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of kategori
+-- ----------------------------
+INSERT INTO `kategori` VALUES (1, 'Pendaftaran');
+INSERT INTO `kategori` VALUES (2, 'Prestasi');
+INSERT INTO `kategori` VALUES (3, 'Berita');
 
 -- ----------------------------
 -- Table structure for kontak
@@ -126,12 +143,13 @@ CREATE TABLE `pendaftar`  (
   `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
   `createdAt` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id_pendaftar`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of pendaftar
 -- ----------------------------
 INSERT INTO `pendaftar` VALUES (2, '12', '123', 'Anjas Wicaksana', 'Bogor', '2001-07-25', '', '2131', 'Islam', 'Nuryati', 'Sartono', '081387387277', '081387387277', 'Laladonn', '2022-02-03 02:57:44');
+INSERT INTO `pendaftar` VALUES (3, '1234', '1234', 'asd', 'asd', '2022-02-15', 'laki-laki', '1234', 'katholik', 'asd', 'asd12', '123', '123', 'asd', '2022-02-09 08:36:10');
 
 -- ----------------------------
 -- Table structure for posts
@@ -141,17 +159,22 @@ CREATE TABLE `posts`  (
   `id_posts` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `alt_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `createdAt` datetime NULL DEFAULT NULL,
   `updatedAt` datetime NULL DEFAULT NULL,
   `visited` int NULL DEFAULT NULL,
+  `meta_keyword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `meta_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `id_kategori` int NULL DEFAULT NULL,
   PRIMARY KEY (`id_posts`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of posts
 -- ----------------------------
-INSERT INTO `posts` VALUES (7, 'Pemakai Narkoba Tertangkap di Kampung Boncos Ternyata Pemain Bola', 'TEMPO.CO, Jakarta - Laga Liga Spanyol antara Barcelona vs Atletico Madrid berakhir dengan skor 4-2. Pelatih Atletico Madrid, Diego Simeone, menilai timnya bermain lebih baik meskipun akhirnya menelan kekalahan karena Barcelona lebih tajam di depan gawang.\r\n\r\nDalam konferensi pers usai pertandingan, Simeone mengakui keunggulan Barcelona pada laga itu. Dia menilai perbedaan kedua tim pada laga itu adalah karena anak asuhnya kurang tajam di depan gawang.\r\n\r\nPelatih asal Argentina itu menilai timnya lebih mengontrol pertandingan tersebut. Menurut dia, mereka juga sebenarnya memiliki peluang untuk mencetak tiga gol lainnya pada babak pertama. Sayangnya peluang yang didapatkan Joao Felix dan Stefan Savic tak berbuah gol.\r\n\r\n\"Anda harus menghargai betapa tajamnya lawan kami dan kami berupaya untuk meningkatkan intensitas, agresifitas dan konsentrasi kami,\" kata Simeone. \"Kami mampu unggul terlebih dahulu, kami lebih mengontol pertandingan secara posisi, bisa membuka ruang untuk Carrasco dan Joao, kami memiliki tiga peluang lainnya pada babak pertama, dua untuk Joao dan satu untuk Savic. Mereka lebih tajam. Di luar penguasaan bola dan serangan balik, tim yang lebih tajam yang akan menang.\"\r\n\r\nDiego Simeone juga menyatakan bahwa mereka tampil lebih baik pada babak kedua dengan berhasil memperkecil ketertinggalan menjadi 2-4. Sayangnya, menurut dia, pertahanan skuad asuhan Xavi Hernandez tampil apik, bahkan setelah mereka bermain dengan 10 orang.\r\n\r\n\"Di babak kedua kami lebih agresif dan intens, kami mengubah skor menjadi 4-2 dan rasanya seperti kami bisa masuk ke dalam permainan. Barca bertahan dan terorganisir dengan baik dan kami memiliki lebih sedikit peluang,\" kata dia.\r\n\r\nPelatih yang musim lalu membawa Atletico Madrid menjuarai Liga Spanyol itu pun menyoroti kinerja lini belakangnya. Menurut dia, musim ini pertahanan mereka tak sebaik satu dekade terakhir.\r\n\r\n\"Kami telah bertahan dengan sangat baik selama sepuluh tahun dan tahun ini kami tidak melakukan apa yang biasanya kami lakukan. Kami harus berkembang, kami memiliki waktu seminggu untuk memperbaikinya sekarang. Kami kurang beruntung dan sedikit kurang fokus juga,\" kata dia.\r\n\r\nPada laga itu, Atletico Madrid memang sempat unggul lewat gol Yannick Carrasco pada menit ke-8. Akan tetapi Barcelona langsung membalasnya dua menit berselang lewat gol Jordi Alba.\r\n\r\nBarca berbalik unggul lewat sundulan Gavi yang memanfaatkan umpan silang Adama Traore pada menit ke-21. Ronald Araujo membuat skor menjadi 3-1 dua menit menjelang babak pertama usai.\r\n\r\nAnak asuh Xavi Hernandez menambah keunggulannya empat menit babak kedua berjalan. Kali ini Dani Alves mencatatkan namanya di papan skor.\r\nAtletico Madrid hanya mampu memperkecil ketertinggalan lewat sundulan Luis Suarez pada menit ke-58. Sebelas menit berselang Barcelona harus bermain dengan 10 orang setelah Dani Alves diganjar kartu merah karena melanggar Carrasco.\r\n\r\nMeskipun bermain dengan keunggulan satu orang, Los Rojiblancos gagal menambah gol. Laga Barcelona vs Atletico Madrid pun berakhir dengan skor 4-2 dan membuat Blaugrana naik ke peringkat keempat klasemen sementara Liga Spanyol. Mereka menyalip Atletico Madrid yang harus turun dengan ke posisi kelima.', 'pkbm_posts_1643602161.jpg', '2022-01-31 05:09:21', NULL, NULL);
+INSERT INTO `posts` VALUES (7, 'Pemakai Narkoba Tertangkap', '<p>TEMPO.CO, Jakarta - Laga Liga Spanyol antara Barcelona vs Atletico Madrid berakhir dengan skor 4-2. Pelatih Atletico Madrid, Diego Simeone, menilai timnya bermain lebih baik meskipun akhirnya menelan kekalahan karena Barcelona lebih tajam di depan gawang. Dalam konferensi pers usai pertandingan, Simeone mengakui keunggulan Barcelona pada laga itu. Dia menilai perbedaan kedua tim pada laga itu adalah karena anak asuhnya kurang tajam di depan gawang.</p>\r\n\r\n<p>Pelatih asal Argentina itu menilai timnya lebih mengontrol pertandingan tersebut. Menurut dia, mereka juga sebenarnya memiliki peluang untuk mencetak tiga gol lainnya pada babak pertama. Sayangnya peluang yang didapatkan Joao Felix dan Stefan Savic tak berbuah gol. &quot;Anda harus menghargai betapa tajamnya lawan kami dan kami berupaya untuk meningkatkan intensitas, agresifitas dan konsentrasi kami,&quot; kata Simeone. &quot;Kami mampu unggul terlebih dahulu, kami lebih mengontol pertandingan secara posisi, bisa membuka ruang untuk Carrasco dan Joao, kami memiliki tiga peluang lainnya pada babak pertama, dua untuk Joao dan satu untuk Savic. Mereka lebih tajam. Di luar penguasaan bola dan serangan balik, tim yang lebih tajam yang akan menang.&quot;</p>\r\n\r\n<p>Diego Simeone juga menyatakan bahwa mereka tampil lebih baik pada babak kedua dengan berhasil memperkecil ketertinggalan menjadi 2-4. Sayangnya, menurut dia, pertahanan skuad asuhan Xavi Hernandez tampil apik, bahkan setelah mereka bermain dengan 10 orang. &quot;Di babak kedua kami lebih agresif dan intens, kami mengubah skor menjadi 4-2 dan rasanya seperti kami bisa masuk ke dalam permainan. Barca bertahan dan terorganisir dengan baik dan kami memiliki lebih sedikit peluang,&quot; kata dia. Pelatih yang musim lalu membawa Atletico Madrid menjuarai Liga Spanyol itu pun menyoroti kinerja lini belakangnya. Menurut dia, musim ini pertahanan mereka tak sebaik satu dekade terakhir.</p>\r\n\r\n<p>&quot;Kami telah bertahan dengan sangat baik selama sepuluh tahun dan tahun ini kami tidak melakukan apa yang biasanya kami lakukan. Kami harus berkembang, kami memiliki waktu seminggu untuk memperbaikinya sekarang. Kami kurang beruntung dan sedikit kurang fokus juga,&quot; kata dia. Pada laga itu, Atletico Madrid memang sempat unggul lewat gol Yannick Carrasco pada menit ke-8. Akan tetapi Barcelona langsung membalasnya dua menit berselang lewat gol Jordi Alba. Barca berbalik unggul lewat sundulan Gavi yang memanfaatkan umpan silang Adama Traore pada menit ke-21. Ronald Araujo membuat skor menjadi 3-1 dua menit menjelang babak pertama usai. Anak asuh Xavi Hernandez menambah keunggulannya empat menit babak kedua berjalan. Kali ini Dani Alves mencatatkan namanya di papan skor. Atletico Madrid hanya mampu memperkecil ketertinggalan lewat sundulan Luis Suarez pada menit ke-58. Sebelas menit berselang Barcelona harus bermain dengan 10 orang setelah Dani Alves diganjar kartu merah karena melanggar Carrasco. Meskipun bermain dengan keunggulan satu orang, Los Rojiblancos gagal menambah gol. Laga Barcelona vs Atletico Madrid pun berakhir dengan skor 4-2 dan membuat Blaugrana naik ke peringkat keempat klasemen sementara Liga Spanyol. Mereka menyalip Atletico Madrid yang harus turun dengan ke posisi kelima.</p>\r\n', '', 'pkbm_posts_1644294519.jpg', '2022-02-08 05:27:54', NULL, 19, 'keyword1,keyword2,keyword3,keyword4', 'TEMPO.CO, Jakarta - Laga Liga Spanyol antara Barcelona vs Atletico Madrid berakhir dengan skor 4-2. Pelatih Atletico Madrid, Diego Simeone, menilai timnya bermain lebih baik meskipun akhirnya menelan kekalahan karena Barcelona lebih tajam di depan gawang...', 1);
+INSERT INTO `posts` VALUES (8, 'Rupiah Rabu pagi menguat 24 poin', '<p>Jakarta (ANTARA) - Ketua Fraksi Partai Demokrat DPR RI Edhie Baskoro Yudhoyono (Ibas) berharap agar pers, sebagai salah satu pilar demokrasi yang memperjuangkan suara rakyat, bersifat jernih, jujur, dan adil dalam memilah saran dan kritik.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&ldquo;Saya percaya bahwa media, pers, dan rakyat Indonesia akan jernih, jujur, dan adil memilah saran dan kritik dalam sinergi bersama,&rdquo; kata Ibas dalam keterangan yang diterima di Jakarta, Rabu.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>Ibas juga mengungkapkan apresiasi dan terima kasihnya kepada insan pers Indonesia pada peringatan Hari Pers Nasional yang jatuh setiap tanggal 9 Februari ini.</p>\r\n', '', 'pkbm_posts_1644376757.jpg', '2022-02-09 04:19:17', NULL, 9, '', '', 3);
 
 -- ----------------------------
 -- Table structure for slider
